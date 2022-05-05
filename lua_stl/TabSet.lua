0a142b842e39cff26f2ea26c9db4da735dccb31d
adan shaw <adan_shaw@qq.com>
2022-05-05 14:15:34 +0800

    Update TabSet.lua

--- lua_stl/TabSet.lua
+++ lua_stl/TabSet.lua
@@ -31,7 +31,7 @@ end
 function TabSet:Front()
 	local Pos = self.Pos
 	local Body = self.body
-	return ((Pos == 0)  and  nil  or  Body[1])
+	return ((Pos <= 0)  and  nil  or  Body[1])
 end
 
 --查看队尾元素(不操作, 只查看)
@@ -39,7 +39,7 @@ end
 function TabSet:Back()
 	local Pos = self.Pos
 	local Body = self.body
-	return ((Pos == 0)  and  nil  or  Body[Pos])
+	return ((Pos <= 0)  and  nil  or  Body[Pos])
 end
 
 --从Back队尾插入
@@ -59,7 +59,7 @@ function TabSet:PushBack( val )
 	--更新Pos
 	self.Pos = Pos
 	Body[Pos] = val
-	return true
+	--return true
 end
 
 --从Back队尾弹出
@@ -67,7 +67,7 @@ end
 function TabSet:PopBack()
 	local Pos = self.Pos
 	local Body = self.body
-	if Pos == 0 then
+	if Pos <= 0 then
 		return nil
 	else
 		Pos = Pos - 1
@@ -94,7 +94,7 @@ function TabSet:PushFront( val )
 	--更新Pos
 	self.Pos = Pos + 1
 	Insert(Body, 1, val)
-	return true
+	--return true
 end
 
 --从Front队头弹出
@@ -104,7 +104,7 @@ function TabSet:PopFront()
 	local ret = nil
 	local Remove = table.remove
 	local Body = self.body
-	if Pos == 0 then
+	if Pos <= 0 then
 		return nil
 	else
 		ret = Body[1]
@@ -115,11 +115,52 @@ function TabSet:PopFront()
 	end
 end
 
+--从Back队尾插入装有n 个元素的连续table(必须保证元素类型一致性, 遵守number/string/anything 原则)
+function TabSet:PushBack_TN( t )
+	local Pos = self.Pos
+	local Body = self.body
+	--必须是'从下标1 开始的连续table', 否则#t 失败
+	local tLen = #t
+	local i = 1
+	local pos = Pos
+	for i=1,tLen,1 do
+		pos = pos + 1
+		Body[pos] = t[i]
+	end
+	--更新Pos
+	self.Pos = pos
+end
+
+--从Front队头弹出装有n 个元素的连续table
+--成功返回table, 失败返回nil
+function TabSet:PopFront_TN( n )
+	local t1, t2 = {}, {}
+	local Pos = self.Pos
+	local Body = self.body
+	local i = 1
+	local tmp = 1
+	--拒绝传入错误值, 要导出全部元素, 请先调用Len()
+	if n > Pos or n <= 0 or Pos <= 0 then
+		return nil
+	end
+	for i=1,n,1 do
+		t1[i] = Body[i]
+	end
+	for i=n+1,Pos,1 do
+		t2[tmp] = Body[i]
+		tmp = tmp + 1
+	end
+	--self.body = nil
+	self.body = t2
+	self.Pos = Pos - n
+	return t1
+end
+
 --栈空?
 --成功返回true, 失败返回false
 function TabSet:Empty()
 	local Pos = self.Pos
-	return ((Pos == 0)  and  true  or  false)
+	return ((Pos <= 0)  and  true  or  false)
 end
 
 --返回栈当前元素的长度
@@ -139,6 +180,26 @@ function TabSet:Sort()
 	local Sort = table.sort
 	local Body = self.body
 	Sort(Body)
+end
+
+--洗牌乱序, 打乱TabQueue 原来的顺序, 随机排序
+function TabSet:Shuffle()
+	local Pos = self.Pos
+	local Body = self.body
+	local Random = math.random
+	local i, count = 1, Pos
+	--n 个元素, 乱序n 次(不行可以增加乱序次数)
+	--local i, count = 1, Pos*2
+	local r1, r2 = 0, 0
+	local tmp = nil
+	for i=1,count,1 do
+		r1 = Random(1, Pos)
+		r2 = Random(1, Pos)
+		--不管r1 是否等于r2, 坚决不用if, 直接交换, 相等也不碍事
+		tmp = Body[r1]
+		Body[r1] = Body[r2]
+		Body[r2] = tmp
+	end
 end
 
 --查找元素(不是pop, 只查找, 不删除)
@@ -214,11 +275,17 @@ end
 --自测函数(不对外公开)
 local function Test(t_set_num)
 	local i = 0
+	local t0 = { 99, 88, 6, 7, 33, 1 }
 	t_set_num:PushFront(19)
 	t_set_num:PushBack(1)
 	t_set_num:PushFront(1900)
 	t_set_num:PushBack(222)
 	t_set_num:InsertPos(2, 666)
+	print("t_set_num.Pos\t=",t_set_num.Pos)
+	for i=1,t_set_num.Pos,1 do
+		print(t_set_num.body[i])
+	end
+	t_set_num:Shuffle()
 	print("t_set_num.Pos\t=",t_set_num.Pos)
 	for i=1,t_set_num.Pos,1 do
 		print(t_set_num.body[i])
@@ -242,6 +309,20 @@ local function Test(t_set_num)
 	print(t_set_num:Empty())
 	t_set_num:Clear()
 	print(t_set_num:Empty())
+	t_set_num:PushBack_TN(t0)
+	print("t_set_num.Pos\t=",t_set_num.Pos)
+	for i=1,t_set_num.Pos,1 do
+		print(t_set_num.body[i])
+	end
+	t0 = t_set_num:PopFront_TN(3)
+	print("t_set_num.Pos\t=",t_set_num.Pos)
+	for i=1,t_set_num.Pos,1 do
+		print(t_set_num.body[i])
+	end
+	print("t0:")
+	for i=1,#t0,1 do
+		print(t0[i])
+	end
 end
 
 --启动自测
