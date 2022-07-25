e0a120d84d15f02e530d6dafca7963e0ddfe2181
adan shaw <adan_shaw@qq.com>
2022-07-25 14:14:06 +0800

    Create hiredis_test.c

---
+++ extern_lib/redis/hiredis_test.c
@@ -0,0 +1,485 @@
+/*
+with the Program (or with a work based on the Program) on a volume of
+a storage or distribution medium does not bring the other work under
+the scope of this License.
+
+  3. You may copy and distribute the Program (or a work based on it,
+under Section 2) in object code or executable form under the terms of
+Sections 1 and 2 above provided that you also do one of the following:
+
+    a) Accompany it with the complete corresponding machine-readable
+    source code, which must be distributed under the terms of Sections
+    1 and 2 above on a medium customarily used for software interchange; or,
+
+    b) Accompany it with a written offer, valid for at least three
+    years, to give any third party, for a charge no more than your
+    cost of physically performing source distribution, a complete
+    machine-readable copy of the corresponding source code, to be
+    distributed under the terms of Sections 1 and 2 above on a medium
+    customarily used for software interchange; or,
+
+    c) Accompany it with the information you received as to the offer
+    to distribute corresponding source code.  (This alternative is
+    allowed only for noncommercial distribution and only if you
+    received the program in object code or executable form with such
+    an offer, in accord with Subsection b above.)
+
+The source code for a work means the preferred form of the work for
+making modifications to it.  For an executable work, complete source
+code means all the source code for all modules it contains, plus any
+associated interface definition files, plus the scripts used to
+control compilation and installation of the executable.  However, as a
+special exception, the source code distributed need not include
+anything that is normally distributed (in either source or binary
+form) with the major components (compiler, kernel, and so on) of the
+operating system on which the executable runs, unless that component
+itself accompanies the executable.
+
+If distribution of executable or object code is made by offering
+access to copy from a designated place, then offering equivalent
+access to copy the source code from the same place counts as
+distribution of the source code, even though third parties are not
+compelled to copy the source along with the object code.
+
+  4. You may not copy, modify, sublicense, or distribute the Program
+except as expressly provided under this License.  Any attempt
+otherwise to copy, modify, sublicense or distribute the Program is
+void, and will automatically terminate your rights under this License.
+However, parties who have received copies, or rights, from you under
+this License will not have their licenses terminated so long as such
+parties remain in full compliance.
+
+  5. You are not required to accept this License, since you have not
+signed it.  However, nothing else grants you permission to modify or
+distribute the Program or its derivative works.  These actions are
+prohibited by law if you do not accept this License.  Therefore, by
+modifying or distributing the Program (or any work based on the
+Program), you indicate your acceptance of this License to do so, and
+all its terms and conditions for copying, distributing or modifying
+the Program or works based on it.
+
+  6. Each time you redistribute the Program (or any work based on the
+Program), the recipient automatically receives a license from the
+original licensor to copy, distribute or modify the Program subject to
+these terms and conditions.  You may not impose any further
+restrictions on the recipients' exercise of the rights granted herein.
+You are not responsible for enforcing compliance by third parties to
+this License.
+
+  7. If, as a consequence of a court judgment or allegation of patent
+infringement or for any other reason (not limited to patent issues),
+conditions are imposed on you (whether by court order, agreement or
+otherwise) that contradict the conditions of this License, they do not
+excuse you from the conditions of this License.  If you cannot
+distribute so as to satisfy simultaneously your obligations under this
+License and any other pertinent obligations, then as a consequence you
+may not distribute the Program at all.  For example, if a patent
+license would not permit royalty-free redistribution of the Program by
+all those who receive copies directly or indirectly through you, then
+the only way you could satisfy both it and this License would be to
+refrain entirely from distribution of the Program.
+
+If any portion of this section is held invalid or unenforceable under
+any particular circumstance, the balance of the section is intended to
+apply and the section as a whole is intended to apply in other
+circumstances.
+
+It is not the purpose of this section to induce you to infringe any
+patents or other property right claims or to contest validity of any
+such claims; this section has the sole purpose of protecting the
+integrity of the free software distribution system, which is
+implemented by public license practices.  Many people have made
+generous contributions to the wide range of software distributed
+through that system in reliance on consistent application of that
+system; it is up to the author/donor to decide if he or she is willing
+to distribute software through any other system and a licensee cannot
+impose that choice.
+
+This section is intended to make thoroughly clear what is believed to
+be a consequence of the rest of this License.
+
+  8. If the distribution and/or use of the Program is restricted in
+certain countries either by patents or by copyrighted interfaces, the
+original copyright holder who places the Program under this License
+may add an explicit geographical distribution limitation excluding
+those countries, so that distribution is permitted only in or among
+countries not thus excluded.  In such case, this License incorporates
+the limitation as if written in the body of this License.
+
+  9. The Free Software Foundation may publish revised and/or new versions
+of the General Public License from time to time.  Such new versions will
+be similar in spirit to the present version, but may differ in detail to
+address new problems or concerns.
+
+Each version is given a distinguishing version number.  If the Program
+specifies a version number of this License which applies to it and "any
+later version", you have the option of following the terms and conditions
+either of that version or of any later version published by the Free
+Software Foundation.  If the Program does not specify a version number of
+this License, you may choose any version ever published by the Free Software
+Foundation.
+
+  10. If you wish to incorporate parts of the Program into other free
+programs whose distribution conditions are different, write to the author
+to ask for permission.  For software which is copyrighted by the Free
+Software Foundation, write to the Free Software Foundation; we sometimes
+make exceptions for this.  Our decision will be guided by the two goals
+of preserving the free status of all derivatives of our free software and
+of promoting the sharing and reuse of software generally.
+
+                            NO WARRANTY
+
+  11. BECAUSE THE PROGRAM IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.  EXCEPT WHEN
+OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+PROVIDE THE PROGRAM "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED
+OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  THE ENTIRE RISK AS
+TO THE QUALITY AND PERFORMANCE OF THE PROGRAM IS WITH YOU.  SHOULD THE
+PROGRAM PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL NECESSARY SERVICING,
+REPAIR OR CORRECTION.
+
+  12. IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+REDISTRIBUTE THE PROGRAM AS PERMITTED ABOVE, BE LIABLE TO YOU FOR DAMAGES,
+INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING
+OUT OF THE USE OR INABILITY TO USE THE PROGRAM (INCLUDING BUT NOT LIMITED
+TO LOSS OF DATA OR DATA BEING RENDERED INACCURATE OR LOSSES SUSTAINED BY
+YOU OR THIRD PARTIES OR A FAILURE OF THE PROGRAM TO OPERATE WITH ANY OTHER
+PROGRAMS), EVEN IF SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGES.
+
+                     END OF TERMS AND CONDITIONS
+
+            How to Apply These Terms to Your New Programs
+
+  If you develop a new program, and you want it to be of the greatest
+possible use to the public, the best way to achieve this is to make it
+free software which everyone can redistribute and change under these terms.
+
+  To do so, attach the following notices to the program.  It is safest
+to attach them to the start of each source file to most effectively
+convey the exclusion of warranty; and each file should have at least
+the "copyright" line and a pointer to where the full notice is found.
+
+    <one line to give the program's name and a brief idea of what it does.>
+    Copyright (C) <year>  <name of author>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+Also add information on how to contact you by electronic and paper mail.
+
+If the program is interactive, make it output a short notice like this
+when it starts in an interactive mode:
+
+    Gnomovision version 69, Copyright (C) year name of author
+    Gnomovision comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type `show c' for details.
+
+The hypothetical commands `show w' and `show c' should show the appropriate
+parts of the General Public License.  Of course, the commands you use may
+be called something other than `show w' and `show c'; they could even be
+mouse-clicks or menu items--whatever suits your program.
+
+You should also get your employer (if you work as a programmer) or your
+school, if any, to sign a "copyright disclaimer" for the program, if
+necessary.  Here is a sample; alter the names:
+
+  Yoyodyne, Inc., hereby disclaims all copyright interest in the program
+  `Gnomovision' (which makes passes at compilers) written by James Hacker.
+
+  <signature of Ty Coon>, 1 April 1989
+  Ty Coon, President of Vice
+
+This General Public License does not permit incorporating your program into
+proprietary programs.  If your program is a subroutine library, you may
+consider it more useful to permit linking proprietary applications with the
+library.  If this is what you want to do, use the GNU Lesser General
+Public License instead of this License.
+*/
+
+
+
+//编译:
+//	gcc hiredis_test.c -g3 -o x -lhiredis
+
+
+#include "hiredis-master/hiredis.h"
+#include "hiredis-master/net.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+
+//redisReply{} 结构体的定义详情(仅存储value, 不存储key; key 是索引值, 需要你自己输入)
+/*
+	typedef struct redisReply {
+			int type;											// REDIS_REPLY_*
+																			 返回值类型
+
+			long long integer;						// The integer when type is REDIS_REPLY_INTEGER
+																			 数值(仅'type=REDIS_REPLY_INTEGER'有效)
+
+			size_t len;										// Length of string
+																			 字符串len长度(仅'type=REDIS_REPLY_ERROR/REDIS_REPLY_STRING'有效)
+
+			char *str;										// Used for both REDIS_REPLY_ERROR and REDIS_REPLY_STRING
+																			 字符串指针(仅'type=REDIS_REPLY_ERROR/REDIS_REPLY_STRING'有效)
+
+			size_t elements;							// number of elements, for REDIS_REPLY_ARRAY
+																			 数组向量len长度(仅'type=REDIS_REPLY_ARRAY'有效)
+
+			struct redisReply **element;	// elements vector for REDIS_REPLY_ARRAY
+																			 数组向量指针(仅'type=REDIS_REPLY_ARRAY'有效)
+	}redisReply;
+
+	其中, type类型有:
+		REDIS_REPLY_STRING:  1
+		REDIS_REPLY_ARRAY:   2
+		REDIS_REPLY_INTEGER: 3
+		REDIS_REPLY_NIL:     4
+		REDIS_REPLY_STATUS:  5
+		REDIS_REPLY_ERROR:   6
+*/
+
+
+
+
+
+//测试string 字符串操作
+bool redis_test_str(redisContext *pCon);
+
+//测试list操作
+bool redis_test_list(redisContext *pCon);
+
+//测试set操作
+bool redis_test_set(redisContext *pCon);
+
+//测试hash操作
+bool redis_test_hash(redisContext *pCon);
+
+
+
+int main(void){
+	redisContext *pCon = NULL;
+	redisReply *pReply = NULL;
+	struct timeval timeout = { 1, 500000 }; // 1.5 seconds 设置连接等待时间
+	const char ip[32]="127.0.0.1";
+	const unsigned short port=6379;
+
+
+
+	//建立redis 链接
+	pCon = redisConnectWithTimeout(ip, port, timeout);
+	//assert(pCon != NULL);//无需断言, 断言也于事无补; 如果出错了, 只能自己差错, 不能顾及那么多杂事;
+	//											因此, 假设redis API 一定返回非空指针!! (如果返回一条NULL指针, 你做什么, 程序都会崩溃的)
+	if(pCon->err){
+		printf("redisConnectWithTimeout() Error: %s\n", pCon->errstr);
+		return -1;
+	}
+
+	//发送redis 命令到server, 选择编号=15 的数据库
+	pReply = redisCommand(pCon, "SELECT 15");
+	if(pReply->type == REDIS_REPLY_ERROR){
+		printf("redisCommand() Error: %s\n", pReply->str);
+		redisFree(pCon);
+		return -1;
+	}
+
+	//测试redis 的各种操作
+	if(redis_test_str(pCon))
+		printf("redis_test_str() test ok!!\n");
+	if(redis_test_list(pCon))
+		printf("redis_test_list() test ok!!\n");
+	if(redis_test_set(pCon))
+		printf("redis_test_set() test ok!!\n");
+	if(redis_test_hash(pCon))
+		printf("redis_test_hash() test ok!!\n");
+
+	//释放redis 链接
+	redisFree(pCon);
+
+	return 0;
+}
+
+
+
+
+
+//测试string 字符串操作
+bool redis_test_str(redisContext *pCon){
+	redisReply *pReply;
+	size_t len;
+	char *reply_str = NULL;
+	long long reply_num = 0;
+
+
+
+	//纯字符串测试
+	pReply = redisCommand(pCon, "set test_str fuckyou");
+	if(pReply->type == REDIS_REPLY_ERROR)
+		goto redis_test_str_failed;
+
+	pReply = redisCommand(pCon, "get test_str");
+	if(pReply->type == REDIS_REPLY_ERROR)
+		goto redis_test_str_failed;
+
+	assert(pReply->type == REDIS_REPLY_STRING);
+	reply_str = pReply->str;
+	len = pReply->len;
+	printf("test_str = %s, len=%d\n", reply_str, len);
+	freeReplyObject(pReply);
+
+
+
+	//数字字符串测试(需要使用atoi()/atol()/atoll() 将字符串转换为int/long/long long 整形)
+	pReply = redisCommand(pCon, "set test_str_num 999");
+	if(pReply->type == REDIS_REPLY_ERROR)
+		goto redis_test_str_failed;
+
+	pReply = redisCommand(pCon, "incr test_str_num");
+	if(pReply->type == REDIS_REPLY_ERROR)
+		goto redis_test_str_failed;
+
+	pReply = redisCommand(pCon, "get test_str_num");
+	if(pReply->type == REDIS_REPLY_ERROR)
+		goto redis_test_str_failed;
+
+	assert(pReply->type == REDIS_REPLY_STRING);
+	reply_num = atoll(pReply->str);
+	printf("test_str_num = %d\n", reply_num);
+	freeReplyObject(pReply);
+	return true;
+
+
+redis_test_str_failed:
+	printf("redisCommand() Error: %s\n", pReply->str);
+	freeReplyObject(pReply);//如果两次释放pReply, 会报错: free(): double free detected in tcache 2, Aborted
+	return false;
+}
+
+
+
+//测试list操作(一次只能pop 出一个元素; 可以删除多个元素, 但不能pop出多个元素; 
+//						每个元素都是字符串; 允许出现相同值的元素[值相同但存储位置和下标不同])
+bool redis_test_list(redisContext *pCon){
+	redisReply *pReply;
+	size_t len;
+	char *reply_str = NULL;
+
+
+
+	pReply = redisCommand(pCon, "lpush test_list fuckyou 999 333 dead");
+	//(元素统一为字符串数据, 就算全为number 也是字符串)
+	//pReply = redisCommand(pCon, "lpush test_list 111 999 333 22 22");
+	if(pReply->type == REDIS_REPLY_ERROR)
+		goto redis_test_list_failed;
+
+	pReply = redisCommand(pCon, "lpop test_list");
+	if(pReply->type == REDIS_REPLY_ERROR)
+		goto redis_test_list_failed;
+
+	assert(pReply->type == REDIS_REPLY_STRING);
+	reply_str = pReply->str;
+	len = pReply->len;
+	printf("test_str = %s, len=%d\n", reply_str, len);
+	freeReplyObject(pReply);
+	return true;
+
+
+redis_test_list_failed:
+	printf("redisCommand() Error: %s\n", pReply->str);
+	freeReplyObject(pReply);
+	return false;
+}
+
+
+
+//测试set操作
+bool redis_test_set(redisContext *pCon){
+	redisReply *pReply;
+	unsigned int tmp = 0;
+
+
+
+	//sadd 成功后, 返回0; 如果出现重复元素, 则覆盖(无原子操作)
+	pReply = redisCommand(pCon, "sadd test_set 999 888 1");
+	pReply = redisCommand(pCon, "sadd test_set 999 fuck 888 you aaa bbb 1");
+	if(pReply->type == REDIS_REPLY_ERROR)
+		goto redis_test_set_failed;
+	else
+		printf("sadd() return-value: %d[type=REDIS_REPLY_INTEGER]\n",pReply->integer);
+
+	//返回set 中的所有元素
+	pReply = redisCommand(pCon, "smembers test_set");
+	if(pReply->type == REDIS_REPLY_ERROR)
+		goto redis_test_set_failed;
+
+	//打印smembers 返回的所有set 元素, 元素类型为:REDIS_REPLY_STRING
+	assert(pReply->type == REDIS_REPLY_ARRAY);
+	printf("print all set smembers:\n");
+	for(tmp=0; tmp<pReply->elements; tmp++)
+		printf("%s\n", pReply->element[tmp]->str);
+	return true;
+
+redis_test_set_failed:
+	printf("redisCommand() Error: %s\n", pReply->str);
+	freeReplyObject(pReply);
+	return false;
+}
+
+
+
+//测试hash操作(field and value 要分开获取, 不能同时获取field and value)
+bool redis_test_hash(redisContext *pCon){
+	redisReply *pReply;
+	unsigned int tmp = 0;
+
+
+
+	pReply = redisCommand(pCon, "hmset test_hash a 1 q 2 a 1 a 2 s 2 fd 4");
+	if(pReply->type == REDIS_REPLY_ERROR)
+		goto redis_test_hash_failed;
+
+	//仅获取hash 中所有的fields(没有value, hmget 才能获取键值对)
+	pReply = redisCommand(pCon, "hkeys test_hash");
+	if(pReply->type == REDIS_REPLY_ERROR)
+		goto redis_test_hash_failed;
+
+	assert(pReply->type == REDIS_REPLY_ARRAY);
+	printf("print all hash keys:\n");
+	for(tmp=0; tmp<pReply->elements; tmp++)
+		printf("%s\n", pReply->element[tmp]->str);
+
+	//根据field, 获取对应的value
+	pReply = redisCommand(pCon, "hmget test_hash a q s fd");
+	if(pReply->type == REDIS_REPLY_ERROR)
+		goto redis_test_hash_failed;
+
+	assert(pReply->type == REDIS_REPLY_ARRAY);
+	printf("print all hash values:\n");
+	for(tmp=0; tmp<pReply->elements; tmp++)
+		printf("%s\n", pReply->element[tmp]->str);
+	return true;
+
+
+redis_test_hash_failed:
+	printf("redisCommand() Error: %s\n", pReply->str);
+	freeReplyObject(pReply);
+	return false;
+}
