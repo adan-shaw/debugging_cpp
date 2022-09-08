e35bebc1b6a68ff8b598168ce33cc2692221536a
adan shaw <adan_shaw@qq.com>
2022-09-08 13:51:18 +0800

    Create mmap匿名私有(相当于大号malloc).c

---
+++ posix_linux/mmap/mmap匿名私有(相当于大号malloc).c
@@ -0,0 +1,52 @@
+//编译:
+//		gcc -g3 my_mmap.h ./mmap匿名私有\(相当于大号malloc\).c -o x 
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/mman.h>
+
+
+
+#define PAGE_COUNT (4096*1)
+
+
+
+//创建一块匿名mmap 私有映射(MAP_PRIVATE + MAP_ANONYMOUS, 相当于大号malloc, 很少会用到)
+void mmap_noname_private(void){
+	char *pmap;
+
+
+
+	//根据指定大小, 创建一块mmap 映射内存, 进程私有, 不可读写, 
+	pmap = mmap(NULL, PAGE_COUNT, PROT_NONE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if(pmap == MAP_FAILED){
+		perror("mmap");
+		return;
+	}
+
+	//改变mmap 映射属性, 允许读写
+	if(mprotect(pmap, PAGE_COUNT, PROT_READ | PROT_WRITE) == -1){
+		perror("mprotect()");
+		return;
+	}
+
+	strncpy(pmap, "hello fuckers", sizeof("hello fuckers"));
+	printf("%s\n", pmap);
+
+	//释放mmap
+	munmap(pmap,PAGE_COUNT);
+
+	return ;
+}
+
+
+
+
+int main(void){
+	mmap_noname_private();
+	return 0;
+}
