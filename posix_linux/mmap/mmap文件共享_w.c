65de58408d907f1eb51c9b786595c722ebe29012
adan shaw <adan_shaw@qq.com>
2022-09-08 16:15:18 +0800

    Update mmap文件共享_w.c

--- posix_linux/mmap/mmap文件共享_w.c
+++ posix_linux/mmap/mmap文件共享_w.c
@@ -1,27 +1,23 @@
 //编译:
-//		gcc -g3 ./mmap文件共享_w.c -o xw ; gcc -g3 ./mmap文件共享_r.c -o xr 
+//		gcc -g3 mmap.h ./mmap文件共享_w.c -o xw ; gcc -g3 mmap.h ./mmap文件共享_r.c -o xr 
 
 
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "mmap.h"
 #include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/mman.h>
 #include <fcntl.h>
 
 
 
 #define MMAP_SHARED_FILE "mmap_shared.file"	//mmap 映射绑定的文件名
 #define MMAP_SHARED_FILE_SIZE (4096)				//mmap 映射总长
-#define MMAP_SHARED_FRAME_SIZE (64)					//mmap 映射帧-节长
 
 
 
 int main(void){
 	int fd, tmp;
 	char* pmap;
+	mmap_frame_t m_frame;
 
 	//主进程写, 其他进度读, 就一定是0664(其他进程是4权限, 子进程是6权限, 自己是6权限)
 	fd = open(MMAP_SHARED_FILE, O_RDWR | O_CREAT, 0664);
@@ -38,12 +34,20 @@ int main(void){
 		return -1;
 	}
 
+	//填充帧
+	m_frame.a = -1;
+	m_frame.b = 999;
+	m_frame.c = 1.1;
+	m_frame.d = 9.9;
+	//帧-节长, 截断式读写, 安全(加换行'\n'和字符串终止符号'\0')
+	strncpy(m_frame.buf, "hello parent, i am your 朋友\n\0", 32);
+
 	//为了防止mmap 溢出, 必须从'tmp+第一帧'开始起跳
 	for(tmp=MMAP_SHARED_FRAME_SIZE;tmp<MMAP_SHARED_FILE_SIZE;tmp+=MMAP_SHARED_FRAME_SIZE){
+		memcpy(pmap,&m_frame,MMAP_SHARED_FRAME_SIZE);
+		//printf("a=%d,b=%d,c=%lf,d=%lf,buf=%s\n",m_frame.a,m_frame.b,m_frame.c,m_frame.d,m_frame.buf);
+		printf("tmp=%d, lseek()=%d\n", tmp, lseek(fd, 0, SEEK_END));
 		pmap += MMAP_SHARED_FRAME_SIZE;
-		//帧-节长, 截断式读写, 安全(加换行'\n'和字符串终止符号'\0')
-		strncpy(pmap, "hello parent, i am your 朋友\n\0", MMAP_SHARED_FRAME_SIZE);
-		printf("tmp=%d, lseek()=%d, %s\n", tmp, lseek(fd, 0, SEEK_END), pmap);
 	}
 
 	munmap(pmap, MMAP_SHARED_FILE_SIZE);
