6e52636782378e032a79c7d9715688affe56e5ab
adan shaw <adan_shaw@aliyun.com>
2022-09-29 18:14:07 +0800

    Update arr_stack_mini.c

--- c_basic/container/arr_stack_mini.c
+++ c_basic/container/arr_stack_mini.c
@@ -2,7 +2,7 @@
 	-nostartfiles = 没有开始函数的.c 源文件file, 
 	-e 表示自定义开始函数(需要用exit(0) 结束自定义开始函数, 否则必然会Segmentation fault溢出)
 	局部编译(方便局部调试):
-		gcc -g3 -nostartfiles arr_stack_mini.h arr_stack_mini.c -e test_as -o x
+		gcc -g3 -nostartfiles arr_stack_mini.h arr_stack_mini.c -e test_as_basic -o x
 */
 
 
@@ -11,29 +11,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define as_size_max4test (64)
 
-
-void test_as(void){
-	unsigned int tmp, count, err_count=0;
+void test_as_basic(void){
+	unsigned int tmp, val;
+	as_type* p;
 	as_t* pas = malloc(sizeof(as_t));
-
+	as_type* buf4test;
+	buf4test = malloc(as_size_max4test * sizeof(as_type));
 
 	//初始化
-	as_init(pas);
+	as_init(pas,as_size_max4test);
+	printf("init栈: cur_size=%d, as_size_max=%d\n", as_cur_size(pas), as_size_max(pas));
+	printf("init栈: cur_size=%d, as_size_max=%d\n", as_cur_size(pas), as_size_max(pas));
+
+
 
 	//填满栈
-	for(tmp=0; tmp<as_buf_max; tmp++)
+	for(tmp=0; tmp<as_size_max(pas); tmp++){
 		as_push(pas, tmp);
-	printf("填满栈: cur_size=%d\n", pas->cur_size);
+		//printf("%d, ", tmp);
+	}
+	printf("填满栈: cur_size=%d, as_size_max=%d\n", as_cur_size(pas), as_size_max(pas));
+
+	//为批量操作保留测试数据
+	memcpy(buf4test, pas->buf, as_size_max4test*sizeof(as_type));
 
 	//弹空栈
-	for(tmp=as_buf_max; tmp>0; tmp--)
-		printf("%d, ", as_pop(pas));
-	printf("\n弹空栈: cur_size=%d\n", pas->cur_size);
+	for(tmp=0; tmp<as_size_max(pas); tmp++){
+		as_pop(pas, val);
+		printf("%d, ", val);
+	}
+	printf("\n弹空栈: cur_size=%d, as_size_max=%d\n", as_cur_size(pas), as_size_max(pas));
 
 
-	printf("test_as() finished, err_count=%d\n",err_count);
+
+
+	//批量插入数据
+	printf("\n批量插入数据: cur_size%d+N%d, as_size_max=%d\n", as_cur_size(pas),as_size_max4test, as_size_max(pas));
+	as_pushN(pas, buf4test, as_size_max4test);
+
+	//批量弹空栈
+	memset(buf4test,0,as_size_max4test);
+	as_popALL(pas, buf4test);
+	//as_pop(pas, val);
+	printf("\n批量弹空栈: cur_size=%d, as_size_max=%d\n", as_cur_size(pas), as_size_max(pas));
+
+
+
+	//优先插入数据as_push_prio1
+	as_push_prio1(pas, 1);
+	printf("\n优先插入数据as_push_prio1:cur_size=%d, as_size_max=%d\n", as_cur_size(pas), as_size_max(pas));
+	as_pop(pas, val);
+	printf("%d, ", val);
+
+	printf("\n优先插入数据as_push_prioN:cur_size=%d, as_size_max=%d\n", as_cur_size(pas), as_size_max(pas));
+	as_push_prioN(pas, buf4test, as_size_max4test);
+	for(tmp=0; tmp<as_size_max(pas); tmp++){
+		as_pop(pas, val);
+		printf("%d, ", val);
+	}
+	//as_pop(pas, val);
+
+	//摧毁栈
+	as_destroy(pas);
 	free(pas);
+
+	free(buf4test);
 	exit(0);
 }
 
