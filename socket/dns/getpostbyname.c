b8f04bae69d8ccdf170a8b2bae4f1baa90584298
adan shaw <adan_shaw@aliyun.com>
2022-10-18 18:00:56 +0800

    Update getpostbyname.c

--- socket/dns/getpostbyname.c
+++ socket/dns/getpostbyname.c
@@ -35,22 +35,6 @@
 
 
 
-//1.打印单个服务对应的端口信息, 所使用的协议信息
-void printservent(struct servent* serv);
-
-//2.getservbyname() + getservbyport()测试函数
-void getservbyname_byport_test(void);
-
-
-
-int main(void){
-	getservbyname_byport_test();
-	return 0;
-}
-
-
-
-//1.打印单个服务对应的端口信息, 所使用的协议信息
 void printservent(struct servent* serv){
 	char** p;
 
@@ -59,73 +43,78 @@ void printservent(struct servent* serv){
 	printf("协议类型 serv_proto: %s\n", serv->s_proto);
 
 	p = serv->s_aliases;
-	while(*p != NULL){
-		printf("服务别名 alias name: %s\n", *p);
-		p++;
-	}
+	while(*p != NULL)
+		printf("服务别名 alias name: %s\n", *p++);
+
 	return;
 }
 
 
 
-//2.getservbyname() + getservbyport()测试函数
-void getservbyname_byport_test(void){
+void getservent_test(void){
 	struct servent* serv;
-	const char* service_name = "service_adan";
-	const char* service_type = "tcp";
-	const int service_port = 9213;
 
-
-
-	printf("1.遍历/etc/services 里面的信息\n\n");
-	//1.遍历/etc/services 里面的信息
-	setservent(1);				//打开/etc/services 并挟持
+	printf("遍历/etc/services 里面的信息\n");
+	setservent(1);					//打开/etc/services 并挟持
 
 	while(1){
 		serv = getservent();
 		if(serv == NULL)
-			break;						//读取结束or 遇到意外终止.
-
-		printservent(serv);	//打印单个struct servent 节点
-		printf("\n");
+			break;							//读取结束or 遇到意外终止.
+		else
+			printservent(serv);	//打印单个struct servent 节点
 	}
 
-	endservent();					//关闭/etc/services 并释放
-	printf("\n\n\n");
+	endservent();						//关闭/etc/services 并释放
+	return;
+}
 
 
 
-	//2.根据服务名和协议类型, 获取端口号
-	printf("2.根据服务名和协议类型, 获取端口号\n\n");
+
+void getservbyname_test(void){
+	struct servent* serv;
+	const char* service_name = "service_adan";
+	const char* service_type = "tcp";
+
+	printf("根据服务名和协议类型, 获取端口号\n");
 	serv = getservbyname(service_name,service_type);
 	if(serv != NULL)
-		printf("getservbyname(%s,%s) = %d\n",\
-				serv->s_name, serv->s_proto, serv->s_port);
-	else{
-		printf("cant found [ 类型为:%s, 名为:%s ] 的服务\n",\
-				service_name, service_type);
-		printf("hstrerror(%d)=%s\n",h_errno,hstrerror(h_errno));
-	}
-	printf("\n\n\n");
+		printf("getservbyname(%s,%s) = %d\n",serv->s_name, serv->s_proto, serv->s_port);
+	else
+		printf("getservbyname() cant found [ 类型为:%s, 名为:%s ]的服务, hstrerror(%d)=%s\n",\
+			service_type, service_name, h_errno, hstrerror(h_errno));
+
+	//不需要释放struct servent*,证明getservbyname()实现过程, struct servent*是内置static变量的.
+	//free(serv);
+	return;
+}
 
 
 
-	//3.根据端口号和协议名, 获取服务名(这个可能需要注册才能查得到)
-	printf("3.根据端口号和协议名, 获取服务名(这个可能需要注册才能查得到)\n\n");
+void getservbyport_test(void){
+	struct servent* serv;
+	const char* service_type = "tcp";
+	const int service_port = 9213;
+
+	printf("根据端口号和协议名, 获取服务名(这个可能需要注册才能查得到)\n");
 	serv = getservbyport(service_port,service_type);
 	if(serv != NULL)
-		printf("getservbyport(%d,%s) = %s\n",\
-				serv->s_port, serv->s_proto, serv->s_name);
-	else{
-			printf("cant found [ 类型为:%s, 端口为:%d ] 的服务\n",\
-					service_type, service_port);
-			printf("hstrerror(%d)=%s\n",h_errno,hstrerror(h_errno));
-		}
-	printf("\n\n\n");
+		printf("getservbyport(%d,%s) = %s\n",serv->s_port, serv->s_proto, serv->s_name);
+	else
+		printf("getservbyport() cant found [ 类型为:%s, 端口为:%d ]的服务, hstrerror(%d)=%s\n",\
+			service_type, service_port, h_errno, hstrerror(h_errno));
 
-
-
-	//4.不需要释放struct servent*,证明getservbyname()实现过程, struct servent*是内置static变量的.
 	//free(serv);
 	return ;
 }
+
+
+
+int main(void){
+	getservent_test();
+	getservbyname_test();
+	getservbyport_test();
+	return 0;
+}
+
