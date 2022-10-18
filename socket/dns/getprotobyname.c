282fe9b854dcf53d0fd4edeebadd43846ccf4f2e
adan shaw <adan_shaw@aliyun.com>
2022-10-18 18:01:34 +0800

    Update getprotobyname.c

--- socket/dns/getprotobyname.c
+++ socket/dns/getprotobyname.c
@@ -36,22 +36,6 @@
 
 
 
-//1.打印单个网络协议信息
-void printproto(struct protoent* proto);
-
-//2.获取<当前主机>所支持的网络协议, 并打印所有的协议信息
-void getprotobyname_test(void);
-
-
-
-int main(void){
-	getprotobyname_test();
-	return 0;
-}
-
-
-
-//1.打印单个网络协议信息
 void printproto(struct protoent* proto){
 	char** p;
 
@@ -59,109 +43,108 @@ void printproto(struct protoent* proto){
 	printf("协议编号 proto number: %d\n", proto->p_proto);
 
 	p = proto->p_aliases;
-	while(*p != NULL){
-		printf("协议别名 alias name: %s\n", *p);
-		p++;
-	}
+	while(*p != NULL)
+		printf("协议别名 alias name: %s\n", *p++);
+
 	return;
 }
 
 
 
-//2.获取<当前主机>所支持的网络协议, 并打印所有的协议信息
-void getprotobyname_test(void){
+void getprotoent_test(void){
 	struct protoent* proto;
 
-
-
-	//1.遍历本地<记录文件>， 并打印所有的协议信息
-	printf("1.遍历本地<记录文件>， 并打印所有的协议信息\n\n");
-	setprotoent(1);				//打开并挟持<记录文件>
-												//注意: 你不挟持文件,可能只会一直读取到第一个,读不到后面的协议
+	printf("遍历本地<记录文件>， 并打印所有的协议信息\n");
+	setprotoent(1);					//打开并挟持<记录文件>
+													//注意: 你不挟持文件,可能只会一直读取到第一个,读不到后面的协议
 
 	while(1){
 		proto = getprotoent();
-		if(proto == NULL)		//如果读到/etc/protocols 文件结束
-			break;						//读取结束or 遇到意外终止
-
-		printproto(proto);	//打印单个struct protoent 节点
-		printf("\n");
+		if(proto == NULL)			//如果读到/etc/protocols 文件结束
+			break;							//读取结束or 遇到意外终止
+		else
+			printproto(proto);	//打印单个struct protoent 节点
 	}
 
-	endprotoent();				//关闭并释放<记录文件>
-	printf("\n\n\n");
+	endprotoent();					//关闭并释放<记录文件>
+	return;
+}
 
 
 
-	//2.打印指定协议名称, 打印协议信息; 不指定参数, 默认获取第一个协议(单点查询不需要打开/etc/protocols)
-	printf("2.打印指定协议名称, 打印协议信息; 不指定参数, 默认获取第一个协议\n\n");
+void getprotobyname_test(void){
+	struct protoent* proto;
+
+	printf("打印指定协议名称, 打印协议信息; 不指定参数, 默认获取第一个协议\n");
 	proto = getprotobyname("ip");	//ip=ipv4, 你可以试试 ipv6
 	if(proto != NULL)
 		printproto(proto);					//打印测试结果
-	else{
-		printf("getprotoent() fail, h_errno = %d\n",h_errno);
-		printf("%s\n",hstrerror(h_errno));
-	}
-	printf("\n\n\n");
+	else
+		printf("getprotoent failed, hstrerror(%d)=%s\n",h_errno,hstrerror(h_errno));
 
-
-
-	//3.不需要释放struct protoent*,证明getprotobyname()实现过程,struct protoent*是内置static变量的.
+	//不需要释放struct protoent*,证明getprotobyname()实现过程,struct protoent*是内置static变量的.
 	//free(proto);
-
 	return ;
+}
+
+
+
+int main(void){
+	getprotoent_test();
+	getprotobyname_test();
+	return 0;
 }
 
 
 
 //协议名称list数组(样板, 提供参考)
 /*
-const char *const protocol_name[]={
-	"ip",
-	"icmp",
-	"igmp",
-	"ggp",
-	"ipencap",
-	"st",
-	"tcp",
-	"egp",
-	"igp",
-	"pup",
-	"udp",
-	"hmp",
-	"xns-idp",
-	"rdp",
-	"iso-tp4",
-	"xtp",
-	"ddp",
-	"idpr-cmtp",
-	"ipv6",
-	"ipv6-route",
-	"ipv6-frag",
-	"idrp",
-	"rsvp",
-	"gre",
-	"esp",
-	"ah",
-	"skip",
-	"ipv6-icmp",
-	"ipv6-nonxt",
-	"ipv6-opts",
-	"rspf",
-	"vmtp",
-	"eigrp",
-	"ospf",
-	"ax.25",
-	"ipip",
-	"etherip",
-	"encap",
-	"pim",
-	"ipcomp",
-	"vrrp",
-	"l2tp",
-	"isis",
-	"sctp",
-	"fc",
-	NULL
-};
+	const char *const protocol_name[]={
+		"ip",
+		"icmp",
+		"igmp",
+		"ggp",
+		"ipencap",
+		"st",
+		"tcp",
+		"egp",
+		"igp",
+		"pup",
+		"udp",
+		"hmp",
+		"xns-idp",
+		"rdp",
+		"iso-tp4",
+		"xtp",
+		"ddp",
+		"idpr-cmtp",
+		"ipv6",
+		"ipv6-route",
+		"ipv6-frag",
+		"idrp",
+		"rsvp",
+		"gre",
+		"esp",
+		"ah",
+		"skip",
+		"ipv6-icmp",
+		"ipv6-nonxt",
+		"ipv6-opts",
+		"rspf",
+		"vmtp",
+		"eigrp",
+		"ospf",
+		"ax.25",
+		"ipip",
+		"etherip",
+		"encap",
+		"pim",
+		"ipcomp",
+		"vrrp",
+		"l2tp",
+		"isis",
+		"sctp",
+		"fc",
+		NULL
+	};
 */
