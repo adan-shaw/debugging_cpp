ef65e852bff7c2b6cc90e98a93a09ebf113716be
adan shaw <adan_shaw@aliyun.com>
2022-12-02 13:25:15 +0800

    Create py全局变量&局部变量.py

---
+++ AI人工智能/cpython基础/py基础编码/py变量定义/py全局变量&局部变量.py
@@ -0,0 +1,81 @@
+#coding=utf-8
+import copy
+
+
+
+# locals()局部变量字典/globals()全局变量字典的简介:
+'''
+	* locals()只读, globals()可读可写;
+	* locals()存在于函数内, globals()存在于'python进程'内;
+	* 多'python进程'之间, globals()互相独立;
+	* 访问locals()/globals() 不会有性能问题, 但'多线程'访问globals() 会有访问冲突问题;
+	* 拷贝locals()/globals() 会有性能问题, 主要消耗是拷贝消耗;
+	* locals()/globals() 返回值是不一样的, globals() 返回一个指针引用, locals() 返回一份浅拷贝copy;
+'''
+
+#
+# globals() 全局变量
+#
+# 查看全局变量字典(包含变量, 子字典, list等)
+print(globals())
+
+
+# 实时访问全局变量(性能不太好)
+print(globals()["__file__"])
+
+
+# 实时新增全局变量(在非函数区定义的变量, 统一都当成全局变量, 跟c/c++/lua 语言一样)
+tmp = 1
+print(globals()["tmp"])
+
+
+# 实时改变全局变量
+globals()["tmp"] = 2
+print(globals()["tmp"])
+
+
+# 引用全局变量
+gVal = globals()
+gVal["tmp"] = 3
+print(gVal["tmp"])
+gVal["tmp"] = 4
+print(globals()["tmp"])
+
+
+# copy浅拷贝全局变量字典
+gVal_copy=globals().copy()
+#gVal_copy=dict(globals())
+gVal_copy["tmp"] = 5
+print(gVal_copy["tmp"])
+print(globals()["tmp"])
+
+
+# copy深拷贝全局变量字典(深拷贝失败)
+'''
+gVal_copy2=copy.deepcopy(globals())
+gVal_copy2["tmp"] = 6
+print(gVal_copy2["tmp"])
+'''
+
+
+
+
+
+# locals() 函数内的局部变量字典(只读)
+def func():
+	# 访问locals()
+	print(locals())
+
+	# 新增locals() 局部变量字典
+	tmp = 1
+	print(locals()["tmp"])
+
+	# 浅拷贝locals() 局部变量字典
+	# (不同于全局变量, locals()/globals() 返回值是不一样的, globals() 返回一个指针引用, locals() 返回一份浅拷贝copy)
+	lVal_copy = locals()
+	lVal_copy["tmp"] = 2
+	print(lVal_copy["tmp"])
+	print(locals()["tmp"])
+
+func()
+
