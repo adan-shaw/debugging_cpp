b7432db32893eb75786adaf5a135534d4a544274
adan shaw <adan_shaw@aliyun.com>
2022-12-04 02:07:59 +0800

    Create py_sys_stdio.py

---
+++ AI人工智能/cpython基础/py基础编码/py解析器env环境变量/py_sys_stdio.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+
+
+
+import sys
+
+
+
+# 初始化标准输出API(提高引用效率)
+stdout=sys.stdout.write
+stdin=sys.stdin.readline
+stderr=sys.stderr.write
+
+
+
+# 标准输出API的使用
+stdout("hello world\n")
+stderr("hello world\n")
+
+stdout("请输入一行(readline):\n")
+tmp=stdin()
+stdout(tmp)
