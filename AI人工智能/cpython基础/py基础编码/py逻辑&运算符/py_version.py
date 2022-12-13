3761813c75d8c2699d073d201ed2df98a049f01b
adan shaw <adan_shaw@aliyun.com>
2022-12-13 13:28:12 +0800

    Create py_version.py

---
+++ AI人工智能/cpython基础/py基础编码/py逻辑&运算符/py_version.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+
+
+
+import sys
+
+# 打印版本字符串全集, 格式如下: sys.version_info(major=3, minor=6, micro=6, releaselevel='final', serial=0)
+print(sys.version_info)
+
+# 打印主要版本号, 2/3
+print(sys.version_info.major)
+
