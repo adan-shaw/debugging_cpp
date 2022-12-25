e94ea0d76fb927b7ba47709e70c07e6b931e6f1b
adan shaw <adan_shaw@aliyun.com>
2022-12-25 16:52:15 +0800

    Create others序列容器.py

---
+++ AI人工智能/cpython基础/py容器&算法/Flat_Sequences平面序列/others序列容器.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+
+
+
+#
+# range(): 返回一个纯数字的list
+#
+# (默认起始=0,终止=10,默认步进=1)
+print(range(10))
+
+# (起始,终止,默认步进=1)
+print(range(1,10))
+
+# (起始,终止,步进)
+print(range(1,10,3))
+
+
+
+
+
+# memoryview() 返回'二进制数据'数组
+v = memoryview(b'abcefg')
+print(v[1])  # b
+print(v[-1]) # g
