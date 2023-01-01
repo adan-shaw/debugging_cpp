7ada7fc86c6670c18cf2a32e7f0f564d3cf08cb9
adan shaw <adan_shaw@aliyun.com>
2023-01-01 20:30:09 +0800

    Create shell递归.sh

---
+++ posix_linux/shell/adan_sh/sh基础编码lib/sh函数与库/shell递归.sh
@@ -0,0 +1,65 @@
+# , , , , ,,, , , , ,,, ,,, , , ,  , , , ,, ,  , , ,, , , , , , ,, , ,, ,, ,
+# text rules simple version for public:
+# !_1.'行-字符长度'测试(how many char in one line):
+# 1111111111111111111111111111111111111111111111111111111111111111111111111
+# 邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵邵
+# *************************************************************************
+# !_2.标题命名规则(headline named rules):
+# 		!_1.xxx:	'level 1' headline
+# 		!_2.xxx:	'level 1' headline
+# 		!_3.xxx:	'level 1' headline
+# 			@_2.xxx:	'level 2' headline
+# 				#_3.xxx:	'level 3' headline
+# !_3.统一使用english US(英文)的标点符号, 作为编码统一标点字符.
+# !_4.文件更改日志(file changed log):
+# 		...
+# , , , , ,,, , , , ,,, ,,, , , ,  , , , ,, ,  , , ,, , , , , , ,, , ,, ,, ,
+
+#!/bin/sh
+
+#
+# 1.shell 中的'函数递归'调用思维:
+#
+# 最简式:(会卡死, 陷入死循环)
+#recursion() {
+#  recursion
+#}
+
+
+
+#
+# 2.shell 中的递归'控制式'
+#
+
+# 2.1 递归执行自己100 次tmp=$(($1+1))
+recursion() {
+	# 累加控制
+	tmp=$(($1+1))
+	# 溢出判断
+	if [ $tmp -lt 100 ];then
+		# 不溢出, 打印回显
+		echo "fuck you $tmp"
+		# 执行递归, 递归recursion()函数
+		recursion $tmp
+	fi
+	return $tmp
+}
+recursion "0"
+
+
+# 2.全局变量控制版本
+
+# 全局变量count
+gCount="0"
+
+recursion2() {
+	gCount=$(($gCount+1))
+	if [ $gCount -lt 100 ]
+	then
+		echo "fuck you2 $gCount"
+		recursion2
+	fi
+	return $gCount
+}
+recursion2
+
