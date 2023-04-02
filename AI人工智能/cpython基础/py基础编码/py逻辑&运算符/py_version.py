5c114c026056f07c51ee5158914e46162327235d
adan shaw <adan_shaw@aliyun.com>
2023-04-03 02:06:13 +0800

    Update py_version.py

--- AI人工智能/cpython基础/py基础编码/py逻辑&运算符/py_version.py
+++ AI人工智能/cpython基础/py基础编码/py逻辑&运算符/py_version.py
@@ -10,3 +10,15 @@ print(sys.version_info)
 # 打印主要版本号, 2/3
 print(sys.version_info.major)
 
+
+
+# 判断python 版本
+if(sys.version_info.major == 3):
+	if(sys.version_info.minor <= 3):
+		print("<=3.3")
+	else:
+		print(">3.3")
+
+if(sys.version_info.major == 2):
+	print("2")
+
