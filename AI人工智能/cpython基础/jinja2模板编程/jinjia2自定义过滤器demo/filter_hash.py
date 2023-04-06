1922fe5e37b8a2b80043c70a523684eb3b2d4787
adan shaw <adan_shaw@aliyun.com>
2023-04-06 12:10:43 +0800

    Create filter_hash.py

---
+++ AI人工智能/cpython基础/jinja2模板编程/jinjia2自定义过滤器demo/filter_hash.py
@@ -0,0 +1,30 @@
+#coding=utf-8
+
+
+
+import hashlib
+
+
+
+def jinja2_filter_hash(value, hash_type="md5"):
+	"""
+	Example filter providing custom Jinja2 filter - hash
+
+	Hash type defaults to 'md5' if one is not specified
+
+	:param value: value to be hashed
+	:param hash_type: valid hash type
+	:return: computed hash as a hexadecimal string
+	"""
+	hash_func = getattr(hashlib, hash_type, None)
+
+	# 提速版
+	computed_hash = hash_func(value.encode("utf-8")).hexdigest()
+	'''
+	if hash_func:
+		computed_hash = hash_func(value.encode("utf-8")).hexdigest()
+	else:
+		raise AttributeError("No hashing function named {hname}".format(hname=hash_type))
+	'''
+	return computed_hash
+
