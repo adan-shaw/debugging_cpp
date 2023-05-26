9830c4926856d75f386ff5c574282ee94801f762
adan shaw <adan_shaw@aliyun.com>
2023-05-26 15:20:31 +0800

    Create udp_cli.py

---
+++ AI人工智能/cpython基础/py内建库/py_socket/udp/udp_cli.py
@@ -0,0 +1,27 @@
+#coding=utf-8
+
+
+
+import socket
+import time
+
+HOST = '127.0.0.1'
+PORT = 8888
+BUF_MAX = 512
+
+# udp
+sfd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0);
+
+print("sendto() to {0} {1}".format(HOST, PORT))
+
+# 同步发送udp 报式报文
+sfd.sendto(b'Hello server, i am client',(HOST, PORT))
+
+# 同步接收udp 报式报文
+data, addr_info = sfd.recvfrom(BUF_MAX)
+print(addr_info)
+print('recvfrom():', repr(data))
+
+time.sleep(1)
+
+sfd.close()
