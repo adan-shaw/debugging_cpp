b89a1421a745eefb3908ad74d7e6333fbecf63bb
adan-shaw <adan@eva>
2023-09-14 19:23:20 +0800

    a

---
+++ AI人工智能/cpython基础/py基础编码/py解析器env环境变量/sitecustomize.py
@@ -0,0 +1,11 @@
+#-*- encoding: UTF-8 -*-
+
+import sys
+
+#set default encoding to utf-8
+sys.setdefaultencoding('utf-8')
+
+
+
+# 一劳永逸的办法(最好用):
+# 	放在Python安装目录下的lib/site-packages目录下, 以后所有第三方库的默认编码都是: utf-8
