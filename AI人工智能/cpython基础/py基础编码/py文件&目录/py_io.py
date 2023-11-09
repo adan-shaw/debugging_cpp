fc224953383055656c589795e54691edcfee5527
adan-shaw <adan@eva>
2023-11-10 00:48:17 +0800

    add python file io demo

---
+++ AI人工智能/cpython基础/py基础编码/py文件&目录/py_io.py
@@ -0,0 +1,136 @@
+#coding=UTF-8
+
+
+
+# io对象操作api:
+'''
+io.open()								打开file
+io.close()							关闭file
+io.flush()							冲刷file内部缓冲, 立刻写盘到file
+io.fileno()							返回一个整型int 的file描述符fd
+io.isatty()							如果file连接到一个终端设备, 则返回True(否则返回False)
+io.next()								返回file下一行
+io.tell()								返回file当前位置pos
+io.truncate(size)				从file的首行首字符开始, 截断size 个字符(无size, 则表示从当前pos位截断)
+io.read(pos)						从file当前位置pos起, 读取size 字节(未给定值or给定负值, 则读取所有)
+io.readline(n)					读取整行, 一共读取n行, 包括 "\n" 字符
+io.readlines()					读取所有行, 返回一个list
+io.write(str)						向file写入一串string字符串
+io.writelines(seque)		向file写入一个list/tuple(换行符需要自己添加, 否则每个元素都粘在一起)
+io.seek(offset,whence)	修改file当前pos 位
+
+'''
+
+
+
+# file对象操作api:
+'''
+fd.readable()						判断fd流是否可读, 是则返回True(不可读返回False)
+fd.writeable()					判断fd流是否可写, 是则返回True
+fd.seekable()						判断fd流支持随机访问(支持修改pos 位), 则返回True
+'''
+
+
+
+import io
+
+
+
+# t为: 文本读写
+# b为: 二进制读写
+fd = open('./py_io.py','rt')
+fd.close()
+
+# buffering=0: 同步io(指定不带缓冲区, 直读)
+fd_sync = io.open("./py_io.py", "rb", buffering=0)
+fd_sync.close()
+
+
+
+
+# 获取/设置python 机默认的file 缓冲区大小(默认8192 kb)
+io.DEFAULT_BUFFER_SIZE
+
+
+
+fd = open('./py_io.py','rt')
+# 修改pos 位(offset=偏移量, whence=起始位)
+'''
+whence:
+	SEEK_SET=0 - 起始位(默认)
+	SEEK_CUR=1 - 当前位
+	SEEK_END=2 - 结束位
+'''
+print(io.SEEK_SET, io.SEEK_CUR, io.SEEK_END)
+# 当前pos 位后移10 type字节
+fd.seek(10, io.SEEK_SET)
+
+"""
+# 判断fd流是否支持随机访问[python3 only]
+if(fd.seekable()):
+	print("supported for random access")
+
+# 判断fd流是否可读[python3 only]
+if(fd.readable()):
+	print("ready to read")
+
+# 判断fd流是否可写[python3 only]
+if(fd.writable()):
+	print("ready to write")
+"""
+
+fd.close()
+
+
+
+fd = open('./py_io.py','rt')
+# 读取size 字节数据, -1=读取全部
+fd.read(-1)
+
+# 逐行读取file [消耗内存较少, 但性能较差]
+line=fd.readline()
+while line:
+	#print(line)
+	line=fd.readline()
+
+# 逐行读取file [实现细节不明确, 慎用, 但精简]
+#for line in open('./py_io.py','rt'):
+	#print(line)
+
+# pos 复位
+fd.seek(0, io.SEEK_SET)
+# 读取file 所有数据, 返回一个list
+plist = fd.readlines()
+for line in plist:
+	print(line)
+
+
+fd.close()
+
+
+
+
+
+# 写数据到file
+fd = open('./tmp4test','wt')
+text="hello world asshole"
+plist=["apple\n","orange\n","banana\n","watermelon\n"]
+
+# 写入一个string
+fd.write(text)
+# 写入一个string list (需要自己添加换行符)
+fd.writelines(plist)
+# 截断文件
+fd.truncate(32)
+
+fd.close()
+
+# 验证读写后的数据, 全部读出回显
+fd = open('./tmp4test','rt')
+plist = fd.readlines()
+for line in plist:
+	print(line)
+fd.close()
+
+
+
---
+++ AI人工智能/cpython基础/py基础编码/py文件&目录/py_os.py
@@ -0,0 +1,75 @@
+#coding=UTF-8
+
+
+
+import os
+import time
+
+
+
+# os 库
+'''
+1.os 库字符串宏
+	os.linesep                        输出当前平台使用的'行终止符'(即换行符号), win下为: "\t\n", Linux下为: "\n"
+	os.pathsep                        输出驱动器分割符(win 专用, 一般是':')
+	os.sep                            输出操作系统特定的路径分隔符, win下为: "\\", Linux下为: "/"
+	os.curdir                         返回当前目录符('.')
+	os.pardir                         获取当前目录的父目录符('..')
+
+	os.name                           输出字符串指示当前使用平台. win->'nt'; Linux->'posix'
+	os.environ                        获取系统环境变量
+
+
+
+2.shell
+	os.system("bash command")         运行shell命令, 结果直接输出到stdout
+	os.popen("bash command")          运行shell命令, 结果保存到python对象中, 可使用readlines() 读出执行结果
+
+
+
+3.目录
+	os.getcwd()                       获取当前工作目录
+	os.chdir("dirname")               改变当前工作目录
+	os.symlink('path_src','path_des') 创建符号链接, 源需绝对路径
+	os.mkdir('dirname')               创建单级目录
+	os.makedirs('dir1/dir2')          可生成多层递归目录(不需要二次mkdir)
+	os.rmdir('dirname')               删除单级空目录, 若目录不为空则无法删除, 报错(专门删空目录)
+	os.removedirs('dirname1')         自动删除空目录: 若目录为空, 则删除, 并递归到上一级目录, 如若也为空, 则删除, 依此类推;(专门删空目录)
+	os.listdir('path')                列出指定目录下的所有文件和子目录, 包括隐藏文件, 并以列表方式打印
+	os.walk()                         自动遍历文件夹(py_os.path.py 有demo)
+
+
+
+4.文件
+	os.access('path',os.W_OK)             检验文件权限模式, 输出True, False(os.X_OK,os.R_OK,os.W_OK)
+	os.chmod('path',os.W_OK)              改变文件权限模式
+	os.tmpfile()                          创建并打开'w+b'一个新的临时文件
+	os.stat('path/filename')              获取文件/目录信息
+	os.remove()                           删除一个文件
+	os.rename("oldname","newname")        重命名文件/目录
+	os.utime('file_name',(before,after))  修改访问时间记录属性
+'''
+
+
+
+# popen() shell 命令执行
+tmp=os.popen("ls")
+print(tmp.readlines())
+
+# 自动遍历文件夹
+print(os.listdir("."))
+
+
+
+# 获取文件的修改时间
+mod_time = os.path.getmtime('./py_os.py')
+print(mod_time)
+
+# 将修改时间转换为时间戳
+mod_timestamp = time.mktime(time.strptime('2020-10-10 10:00:00', '%Y-%m-%d %H:%M:%S'))
+
+# 篡改文件的访问和修改时间(篡改后, gedit 编辑器会显示文件已经修改, 要求你reload)
+os.utime('./py_os.py', (mod_timestamp, mod_timestamp))
+print(os.path.getmtime('./py_os.py'))
+
+
