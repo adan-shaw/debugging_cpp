5ae143fc4b15460893e3aa3f1800a5687f7b6a9c
adan-shaw <adan@eva>
2024-02-14 23:11:10 +0800

    update libpcap

--- extern_lib/libpcap/demo/icmp_r_pcap.c
+++ extern_lib/libpcap/demo/icmp_r_pcap.c
@@ -7,12 +7,12 @@
 
 
 
-void print_icmp_packet (const u_char * packet, int ts, unsigned int packet_len)
+void print_icmp_packet (const u_char * packet, const struct pcap_pkthdr *header)
 {
 	struct ip *ip_hdr = (struct ip *) (packet + sizeof (struct ether_header));
 	struct icmp *icmp_hdr = (struct icmp *) (packet + sizeof (struct ether_header) + sizeof (struct ip));
 
-	printf ("[%ld ", ts);
+	printf ("[%ld-%ld", header->ts.tv_sec, header->ts.tv_usec);
 	printf ("ICMP] Type: %d", icmp_hdr->icmp_type);
 	printf (" Code: %d", icmp_hdr->icmp_code);
 	printf (" Checksum: %04x", ntohs (icmp_hdr->icmp_cksum));
@@ -20,41 +20,63 @@ void print_icmp_packet (const u_char * packet, int ts, unsigned int packet_len)
 	printf (" Sequence: %04x\n", ntohs (icmp_hdr->icmp_seq));
 }
 
+int check_alive_eth_dev(void){
+	char errbuf[PCAP_ERRBUF_SIZE];
+	char *dev = pcap_lookupdev(errbuf);
+	if (dev == NULL) {
+		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+		return -1;
+	}
+	return 0;
+}
+
+#define LIBPCAP_PACKET_MAX (576)
+
 int main (void)
 {
 	char errbuf[PCAP_ERRBUF_SIZE];
-	pcap_t *handle;//pcap sfd 描述体(每个pcap 操作都离不开这个描述体, 相当于sfd;)
-	struct bpf_program fp;
+	struct bpf_program fp;//过滤规则info 容器
+	const char filter_exp[] = "icmp";
+	const char dev_name[] = "lo";
 	bpf_u_int32 mask;
-	int ts;
-	unsigned int packet_len;
-	u_char packet[65535];
+	struct pcap_pkthdr header;
+	const u_char *packet;
 
-	// Open the network interface for capturing
-	handle = pcap_open_live ("eth0", 65535, 1, 1000, errbuf);
+	// 以混杂模式, 打开网络设备"lo"(回环网络) [第三参数=1, 表示混杂模式], 返回pcap_t 通用sfd info容器(libpcap 专用)
+	pcap_t *handle = pcap_open_live (dev_name, LIBPCAP_PACKET_MAX, 1, 1000, errbuf);
 	if (handle == NULL)
 	{
-		fprintf (stderr, "pcap_open_live() failed: %s\n", errbuf);
-		return 1;
+		if(check_alive_eth_dev() == -1)
+			printf("check_alive_eth_dev() failed!!");
+		fprintf (stderr, "pcap_open_live() failed: [%s]-%s\n", dev_name, errbuf);
+		return -1;
 	}
 
-	// Compile and apply the filter to capture only ICMP packets
-	pcap_compile (handle, &fp, "icmp", 0, mask);
-	pcap_setfilter (handle, &fp);
+	// 生成过滤规则: 专门过滤icmp [Compile the filter to capture only ICMP packets]
+	if(pcap_compile (handle, &fp, filter_exp, 0, mask) == -1) {
+		fprintf(stderr, "Couldn't compile filter: [%s]-%s\n", dev_name, errbuf);
+		return -1;
+	}
 
-	// Capture and print each ICMP packet
+	// 设置过滤规则生效 [Apply the filter to capture]
+	if(pcap_setfilter (handle, &fp) == -1) {
+		fprintf(stderr, "Couldn't apply filter: [%s]-%s\n", dev_name, errbuf);
+		return -1;
+	}
+
+	// 抓包, 并打印每一帧ICMP 包 [ Capture and print each ICMP packet ]
 	while (1)
 	{
-		if (pcap_next_ex (handle, &packet, &packet_len) == 0)
+		// 单次抓包libpcap 抓包, 没有回调函数, 直接取出数据buf 进行处理
+		if ((packet = pcap_next(handle, &header)) == NULL)
 		{
-			break;										// End of capture or error occurred
+			// 一旦出错, 表示读取已经结束[End of capture or error occurred]
+			break;
 		}
-		ts = pcap_gettime ();				// Get the timestamp of the packet capture
-		print_icmp_packet (packet, ts, packet_len);	// Print the ICMP packet details
+		print_icmp_packet (packet, &header);
 	}
 
-	// Cleanup resources
-	pcap_freecode (&fp);					// Free the compiled filter program
-	pcap_close (handle);					// Close the network interface handle
+	pcap_freecode (&fp);//释放过滤规则info 容器
+	pcap_close (handle);//释放pcap_t 通用sfd info容器(libpcap 专用)
 	return 0;
 }
--- extern_lib/libpcap/demo/icmp_s_pcap.c
+++ extern_lib/libpcap/demo/icmp_s_pcap.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pcap.h>
+
 #include <netinet/ip_icmp.h>
+#include <linux/in.h>
+
+
+#define LIBPCAP_PACKET_MAX (576)
 
 int main (void)
 {
@@ -11,49 +16,46 @@ int main (void)
 	struct in_addr target_addr;
 	struct icmp *icmp;
 	struct timeval ts;
-	unsigned char packet[1500];
+	unsigned char packet[LIBPCAP_PACKET_MAX];
 	int packet_len;
 
-	// 获取网络设备
-	dev = pcap_lookupdev (errbuf);
-	if (dev == NULL)
+	// 获取可用的网络设备
+	if ((dev = pcap_lookupdev (errbuf)) == NULL)
 	{
 		fprintf (stderr, "Device not found: %s\n", errbuf);
-		return (2);
+		return -1;
 	}
 
-	// 打开设备
-	handle = pcap_open_live (dev, BUFSIZ, 1, 1000, errbuf);
+	// 以混杂模式, 打开网络设备"lo"(回环网络) [第三参数=1, 表示混杂模式], 返回pcap_t 通用sfd info容器(libpcap 专用)
+	handle = pcap_open_live (dev, LIBPCAP_PACKET_MAX, 1, 1000, errbuf);
 	if (handle == NULL)
 	{
 		fprintf (stderr, "Error opening device %s: %s\n", dev, errbuf);
-		return (2);
+		return -1;
 	}
 
-	// 设置目标IP地址
-	target_addr.s_addr = inet_addr ("192.168.1.1");	// 替换为你想要发送ping请求的目标IP地址
+	// 设置目标IP地址(ping 需要ip 地址, 但不需要指定端口)
+	target_addr.s_addr = inet_addr ("127.0.0.1");
 
-	// 创建ICMP回显请求数据包
+	// 填充icmp 数据包
 	icmp = (struct icmp *) packet;
-	icmp->icmp_type = ICMP_ECHO;	// 回显请求类型
-	icmp->icmp_id = getpid ();		// 当前进程ID作为标识符
-	icmp->icmp_seq = 1;						// 序列号(可选)
-	gettimeofday (&ts, NULL);			// 获取当前时间戳
+	icmp->icmp_type = ICMP_ECHO;			// 回显请求类型
+	icmp->icmp_id = getpid ();				// 当前进程ID作为标识符
+	icmp->icmp_seq = 1;								// 序列号(可选)
+
+	gettimeofday (&ts, NULL);					// 获取当前时间戳
+	packet_len = sizeof (struct icmp);// 填充后的数据包长度(不包括校验和)
 	memcpy (&icmp->icmp_cksum, &ts, sizeof (struct timeval));	// 将时间戳填充到校验和字段中(可选)
-	memset ((char *) packet + sizeof (struct icmp), 0, BUFSIZ - sizeof (struct icmp));	// 填充零到数据部分(可选)
-	packet_len = sizeof (struct icmp);	// 填充后的数据包长度(不包括校验和)
+	icmp->icmp_cksum = csum(packet, packet_len);			// 计算ICMP数据包的校验和(可选)
+	memset ((char *) packet + sizeof (struct icmp), 0, LIBPCAP_PACKET_MAX - sizeof (struct icmp));// 剩余部分数据, 填充为0(可选, 不填充也行)
 
-	// 计算ICMP数据包的校验和(可选)
-	// icmp->icmp_cksum = pcap_checksum(packet, packet_len, NULL);
-
-	// 发送ICMP回显请求数据包
+	// 循环发送ICMP回显请求数据包, 会自动调用pcap_sendpacket 回调函数, 进行icmp 数据发送
 	if (pcap_loop (handle, 1, pcap_sendpacket, NULL) < 0)
 	{
-		fprintf (stderr, "pcap_loop() failed: %s\n", pcap_geterr (handle));
-		return (2);
+		fprintf (stderr, "pcap_loop() failed: [%s]-%s\n", dev, pcap_geterr (handle));
+		return -1;
 	}
 
-	// 关闭设备句柄和释放资源
 	pcap_close (handle);
 	return 0;
 }
