51ae5906a03254e60596a902cdbc2327a7f7625e
adan-shaw <adan@eva>
2024-03-12 15:59:15 +0800

    update udp basic && MTU

--- socket/sock_io/MTU分析test.c
+++ socket/sock_io/MTU分析test.c
@@ -5,50 +5,31 @@
 
 
 
-//IP头(MAX=60字节) + [UDP头(max=8字节) / TCP头(max=60字节)]
+//1.UDP / TCP 报头大小考虑
 /*
+	IP头(MAX=60字节) + [UDP头(max=8字节) / TCP头(max=60字节)]
+
 	IP头加上选项最长60 字节, 最短20字节;
 	附加选项40字节, 用来做ip 路由路径指定, 每一跳消耗32bit=4字节, 最高支持指定10跳;
 
 	UDP头固定8 字节;
 
 	TCP头加上选项最长60 字节, 最短20字节;
-	附加选项40字节, 用来做窗口扩大, 时间戳等操作, 没有比TCP更长的传输控制协议了, ICMP,IGMP都不够TCP长;
+	附加选项40字节, 用来做窗口扩大, 时间戳等操作, 没有比TCP更长的传输控制协议了, ICMP, IGMP都没有TCP 报头长;
+	[此时还不需要考虑ETH 报头]
+	#ETH 报头, 固定28 字节(6字节 48bit的MAC 地址)
 
-	[此时还不需要考虑ARP 报头]
-	#ARP 报头, 固定28 字节(6字节 48bit的MAC 地址)
-
-	ip+tcp报头, 最大容量是: 60+60=120字节, 加上ARP 是120+28=148 字节;
-	ip+tcp报头, 最小容量是: 20+20=40字节, 加上ARP 是40+28=68 字节;
-	ip+udp报头, 最大容量是: 60+8=68字节, 加上ARP 是68+28=96 字节;
-	ip+udp报头, 最小容量是: 20+8=28字节, 加上ARP 是28+28=56 字节;
-	一般可以使用贪婪算法, 使用mini版的内存buf max, 性能更好(ip/tcp 报头附加选项, 很少用);
-	最高容量:
-		60+60=120, 最接近的对齐内存是128字节
-		120+28=148, 最接近的对齐内存是152字节
+	ip+tcp报头, 最大容量是: 60+60=120字节, 加上ETH 是120+28=148 字节;
+	ip+tcp报头, 最小容量是: 20+20=40字节, 加上ETH 是40+28=68 字节;
+	ip+udp报头, 最大容量是: 60+8=68字节, 加上ETH 是68+28=96 字节;
+	ip+udp报头, 最小容量是: 20+8=28字节, 加上ETH 是28+28=56 字节;
 
 	所以, 在互联网写应用, udp 真的很节省!!
 */
 
 
 
-//TCP/UDP 报文长度min, max
-#define mtu_udp_min (28)
-#define mtu_udp_max (68)
-#define mtu_tcp_min (40)
-#define mtu_tcp_max (120)
-
-
-
-//inet 互联网MTU(ARP 报头是加插的, 在以太局域网中需要考虑, inet互联网中不需要考虑)
-#define mtu_inet_udp_min (576-mtu_udp_min)
-#define mtu_inet_udp_max (576-mtu_udp_max)
-#define mtu_inet_tcp_min (576-mtu_tcp_min)
-#define mtu_inet_tcp_max (576-mtu_tcp_max)
-
-
-
-//socket io 数据缓冲区max
+//2.MTU 考虑
 /*
 	以太网封装的数据部分帧长为1500-46字节,最小长度为46字节,6+6+2+46+4,不足46字节,则填空字节补足;
 	标准802.2 MTU:       1492字节;
@@ -61,31 +42,34 @@
 	 但MTU 包括了ip 报头, 所以会不会被切片为MTU 的粒度, 计算时, 还得把ip 报头也计算在内)
 */
 
+//TCP/UDP 报文长度min, max
+#define mtu_udp_min (28)
+#define mtu_udp_max (68)
+#define mtu_tcp_min (40)
+#define mtu_tcp_max (120)
+
+//inet 互联网MTU(ARP 报头是加插的, 在以太局域网中需要考虑, inet互联网中不需要考虑)
+#define mtu_inet_udp_min (576-mtu_udp_max)
+#define mtu_inet_udp_max (576-mtu_udp_min)
+#define mtu_inet_tcp_min (576-mtu_tcp_max)
+#define mtu_inet_tcp_max (576-mtu_tcp_min)
+
 //以太网802.2 MTU
-#define mtu_8022_udp_min (1492-28-mtu_udp_min)
-#define mtu_8022_udp_max (1492-28-mtu_udp_max)
-#define mtu_8022_tcp_min (1492-28-mtu_tcp_min)
-#define mtu_8022_tcp_max (1492-28-mtu_tcp_max)
-
-
+#define mtu_8022_udp_min (1492-28-mtu_udp_max)
+#define mtu_8022_udp_max (1492-28-mtu_udp_min)
+#define mtu_8022_tcp_min (1492-28-mtu_tcp_max)
+#define mtu_8022_tcp_max (1492-28-mtu_tcp_min)
 
 //以太网802.3 MTU
-#define mtu_8023_udp_min (1500-28-mtu_udp_min)
-#define mtu_8023_udp_max (1500-28-mtu_udp_max)
-#define mtu_8023_tcp_min (1500-28-mtu_tcp_min)
-#define mtu_8023_tcp_max (1500-28-mtu_tcp_max)
-
-
+#define mtu_8023_udp_min (1500-28-mtu_udp_max)
+#define mtu_8023_udp_max (1500-28-mtu_udp_min)
+#define mtu_8023_tcp_min (1500-28-mtu_tcp_max)
+#define mtu_8023_tcp_max (1500-28-mtu_tcp_min)
 
 //单次发送的最大长度, 既然使用了限定MTU, 这里可以忽略
-#define mtu_data_max_half (32767)	//IP协议,单次发送操作的max的一半:2^16-1=65536-1,一半为32768-1
-#define mtu_data_max_err (65536)	//sendto() fail, errno = 90;
-#define mtu_data_max (65500)			//udp 单次发送操作的max(不能再大了, 再大请自己分次发送)
-
-
-
-//有多少个帧, 最大接收/发送的数据长度=MTU*frame_size_max;
-#define frame_size_max (1024)
+#define mtu_data_max_half (32767)			//IP协议,单次发送操作的max的一半: 2^16-1=65536-1, 一半为32768-1
+#define mtu_data_max_err (65536)			//udp sendto() fail, errno = 90;(ip 包太大, sendto() 会报错!!)
+#define mtu_data_max (65500)					//udp sendto() 单次发送操作的max(不能再大了, 再大请自己分次发送)
 
 
 
@@ -95,20 +79,18 @@ int main(void){
 	printf("mtu_inet_udp_min=%d\n", mtu_inet_udp_min);
 	printf("mtu_inet_udp_max=%d\n", mtu_inet_udp_max);
 	printf("mtu_inet_tcp_min=%d\n", mtu_inet_tcp_min);
-	printf("mtu_inet_tcp_min=%d\n", mtu_inet_tcp_min);
+	printf("mtu_inet_tcp_max=%d\n", mtu_inet_tcp_max);
 
 	printf("mtu_8022_udp_min=%d\n", mtu_8022_udp_min);
 	printf("mtu_8022_udp_max=%d\n", mtu_8022_udp_max);
 	printf("mtu_8022_tcp_min=%d\n", mtu_8022_tcp_min);
-	printf("mtu_8022_tcp_min=%d\n", mtu_8022_tcp_min);
+	printf("mtu_8022_tcp_max=%d\n", mtu_8022_tcp_max);
 
 	printf("mtu_8023_udp_min=%d\n", mtu_8023_udp_min);
 	printf("mtu_8023_udp_max=%d\n", mtu_8023_udp_max);
 	printf("mtu_8023_tcp_min=%d\n", mtu_8023_tcp_min);
-	printf("mtu_8023_tcp_min=%d\n", mtu_8023_tcp_min);
+	printf("mtu_8023_tcp_max=%d\n", mtu_8023_tcp_max);
 	return 0;
 }
-
-
 
 
---
+++ socket/udp/src_demo/src同步demo/udp_io.c
@@ -0,0 +1,175 @@
+//udp 异步太复杂, 暂时不搞(有需要的话, 请自己动手实现)
+/*
+	udp 异步需要实现:
+		* 异步udp_accept()
+		* udp_send()
+		* udp_recv()
+		* udp_write()
+		* udp_read()
+	这些函数都需要进行异步改造, 比较麻烦复杂, 可以参考网易云风skynet, 更现成, 直接的代码, 参考价值更高;
+	本教材只做原理演示, 属于教学性质;
+*/
+
+
+
+//同步: 根据ip & port阻塞发送一次数据(一旦阻塞, 线程会被挂起), 成功返回发送的数据size, 失败返回-1
+int udp_send(int sfd, const char* ip, unsigned short port, const char* pbuf, int data_len){
+	int tmp;
+	struct sockaddr_in addr;
+	//bzero(&addr, sizeof(struct sockaddr_in));
+	addr.sin_addr.s_addr = inet_addr(ip);
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+
+	tmp = sendto(sfd, pbuf, data_len, 0, (struct sockaddr*)&addr, sizeof(addr));
+	if(tmp > 0)
+		return tmp;//发送成功(非发送成功, 就应该close(sfd))
+	if(tmp == 0){
+		//if(data_len == tmp)//data_len == tmp == 0
+		if(data_len == 0)
+			return tmp;
+		else
+			return -1;//对端关闭, socket 数据没有发出去
+	}
+	//此时, tmp == -1
+	perror("sendto()");
+	return -1;
+}
+
+//同步: 根据ip & port阻塞接收一次数据(一旦阻塞, 线程会被挂起), 成功返回发送的数据size, 失败返回-1
+int udp_recv(int sfd, const char* ip, unsigned short port, const char* pbuf, int buf_len){
+	int tmp;
+	struct sockaddr_in addr;
+	//bzero(&addr, sizeof(struct sockaddr_in));
+	addr.sin_addr.s_addr = inet_addr(ip);
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(port);
+
+	tmp = recvfrom(sfd, *pbuf, buf_len, 0, (struct sockaddr*)&addr, sizeof(addr));
+	if(tmp > 0)
+		return tmp;//接收成功(非接收成功, 就应该close(sfd))
+	if(tmp == 0){
+		//if(buf_len == tmp)//buf_len == tmp == 0 传入的数据长度为0 (虽然极度不推荐这样做, 但既然传入了, 逻辑上也要返回0)
+		if(buf_len == 0)
+			return tmp;
+		else
+			return -1;//对端关闭, socket 什么数据都没有接收到
+	}
+	//此时, tmp == -1
+	perror("recvfrom()");
+	return -1;
+}
+
+
+
+//同步: 向一个已执行bind() + connect() 的udp sfd 进行write() 操作
+int udp_write(int sfd, const char* pbuf, int data_len){
+	int tmp = write(sfd, pbuf, data_len);
+	if(tmp > 0)
+		return tmp;//发送成功(非发送成功, 就应该close(sfd))
+	if(tmp == 0){
+		//if(data_len == tmp)//data_len == tmp == 0
+		if(data_len == 0)
+			return tmp;
+		else
+			return -1;//对端关闭, socket 数据没有发出去
+	}
+	//此时, tmp == -1
+	perror("write()");
+	return -1;
+}
+
+//同步: 向一个已执行bind() + connect() 的udp sfd 进行read() 操作
+int udp_read(int sfd, const char* pbuf, int buf_len){
+	int tmp = read(sfd, *pbuf, buf_len);
+	if(tmp > 0)
+		return tmp;//接收成功(非接收成功, 就应该close(sfd))
+	if(tmp == 0){
+		//if(buf_len == tmp)//buf_len == tmp == 0 传入的数据长度为0 (虽然极度不推荐这样做, 但既然传入了, 逻辑上也要返回0)
+		if(buf_len == 0)
+			return tmp;
+		else
+			return -1;//对端关闭, socket 什么数据都没有接收到
+	}
+	//此时, tmp == -1
+	perror("read()");
+	return -1;
+}
+
+
+
+
+//1.io 操作错误时, 返回的errno 值的意思
+/*
+	case EAGAIN:					//套接字已标记为非阻塞, 网卡正忙, 稍后再试
+
+	case ECONNREFUSE:			//远程主机阻绝网络连接
+	case ENOTCONN:				//与对端套接字尚未被连接上
+
+	case EFAULT:					//内存空间访问出错
+	case ENOMEM:					//内存不足
+	case EINTR:						//操作被信号中断
+	case EINVAL:					//参数无效
+
+	case EBADF:						//sfd不是有效的描述符
+	case ENOTSOCK:				//sfd不是套接字(当返回值是0时, 为正常关闭连接)
+*/
+
+
+
+//2.tcp io缓冲区特性
+/*
+	1.I/O缓冲区在每个TCP套接字中单独存在;
+	2.I/O缓冲区在创建套接字时自动生成;
+	3.即使关闭套接字也会'继续传送输出缓冲区'中遗留的数据;
+	4.关闭套接字将'丢失输入缓冲区'中的数据.
+
+	输入输出缓冲区的默认大小一般都是8K,可以通过 getsockopt()函数获取.
+	如果设置了SO_DONTLINGER, socket 关闭, 就不会再发送数据了, 缓冲区也会随之释放(当然关闭时, 没有io 任务, socket 也会立即释放);
+*/
+
+
+
+//3.io 零拷贝技术
+/*
+	socket 的io 零拷贝技术, 不只是sendfile() 一个, 
+	writev()/sendmmsg(), mmap() 等, 都可以实现零拷贝技术, 跟sendfile() 一样;
+*/
+
+
+
+//4.socket 异步模式
+/*
+	异步模式下, errno=EAGAIN, 表示网卡设备忙(ETH 以太网载波侦听的物理特性, 再次展现);
+	这时候, 再次尝试socket io可能会成功, 网卡可能会空闲, 
+	因此如果socket io 操作返回errno=EAGAIN, 则可以再试;
+*/
+
+
+
+//5.tdp/udp 的最简单报头设计
+/*
+* tcp 报头 + len
+* udp 报头 + count
+*/
+
+
+
+//6.tcp/udp 发送大数据包的区别
+/*
+	如果write()写入的数据大于socket缓冲区,则:
+	TCP:
+		1,阻塞fd: 超过MTU数据会自动分片, 阻塞直到发送完毕(无最大长度限制);
+		2,非阻塞fd: 超过MTU数据会自动分片, 将会返回-1, errno=EAGAIN, 设备自动表示正忙, 正在占用;
+
+	UDP:
+		1,阻塞fd: 如果超过UDP最大包限制将会出错返回-1;
+		2,非阻塞fd: 如果网卡忙, 返回-1, errno=EAGAIN;
+
+	另外:
+		由于socket 的读写缓冲区是分立的, 且默认一个socket 同一时间, 只能有一个io 操作在进行,
+		所以, 理论上可以'默认单次最大'写入8kb 数据,
+		(但如果写入时, 网卡未空闲, 上一次数据未发出去, 下一波数据又来了, 这样的操作越多, '资源竞争冲突'会越严重!!
+		 虽然网卡io 的速度是很快的, 很快可以将数据发出去, 你几乎感觉不出来区别;
+		 但还是要极力避免的'资源竞争冲突', 提高网络程序的并发性能)
+*/
