316cead96d2ec467592b59342bbab325f416d53d
adan-shaw <adan_shaw@qq.com>
2024-05-28 17:39:50 +0800

    update xml

--- extern_lib/serialization/xml/tinyxml2编码/tinyxml2.cpp
+++ extern_lib/serialization/xml/tinyxml2编码/tinyxml2.cpp
@@ -33,19 +33,20 @@ void cre_xml_data_dom(void){
 
 
 
-	declaration = doc.NewDeclaration();							//新建xml doc声明
-	doc.InsertFirstChild(declaration);							//将xml doc声明插入头部
+	declaration = doc.NewDeclaration();								//新建xml doc声明
+	doc.InsertFirstChild(declaration);								//将xml doc声明插入头部
 
-	root=doc.NewElement("Response");								//新建元素
-	doc.InsertEndChild(root);												//将元素插入xml_doc插入尾部
+	root=doc.NewElement("Response");									//新建元素, 并设置元素名为: Response
+	doc.InsertEndChild(root);													//将元素插入xml_doc插入尾部
 
-	type = doc.NewElement("Type");
-	type->SetText("success");
-	root->InsertEndChild(type);
+	type = doc.NewElement("Type");										//新建元素, 并设置元素名为: Type
+	type->SetText("success");													//设置元素的内容
+	type->SetAttribute("id","1234");									//设置元素的属性
+	root->InsertEndChild(type);												//将元素Type 设置为元素Response 的子元素
 
-	doc.Print(&printer);														//将xml_doc 的数据, 打印(保存)到Xmlprint类中
-	str = printer.CStr();														//printer.CStr() 转换成const char*类型
-	std::cout<<printer.CStr()<<std::endl;						//printer.CStr() 直接打印
+	doc.Print(&printer);															//将xml_doc 的数据, 打印(保存)到Xmlprint类中
+	str = printer.CStr();															//printer.CStr() 转换成const char*类型
+	std::cout << printer.CStr() << std::endl;					//printer.CStr() 直接打印到stdout
 
 	return;
 }
@@ -68,23 +69,46 @@ void parse_xml_dom(const char* xml_file_path){
 		return;
 	}
 
-	root = doc.RootElement();												//获取xml root 树根
+	root = doc.RootElement();													//获取xml root 树根
 
-	son = root->FirstChildElement("node");					//获取第一个子元素
-
+	// 遍历打印root xml根 begin (不支持三层结构, 只支持两层结构, 太复杂真的不好定义, 深度太深, 太麻烦!!)
+	printf("<%s>\n", root->Name());
+	son = root->FirstChildElement();									//获取第一个子元素
 	while(son){
-		attr = son->FirstAttribute();									//获取该子元素的属性
-		printf("%s:%s\n",attr->Name(),attr->Value());	//打印该子元素的名字and 值
+		printf("<%s", son->Name());											//打印子元素的name
 
-		son_child = son->FirstChildElement();					//获取该子元素的第一个孙元素
-		while(son_child){															//遍历该子元素的孙元素
-			content = son_child->GetText();
-			printf("%s\n", content);										//打印孙元素的值
-
-			son_child = son_child->NextSiblingElement();//下一个孙元素
+		attr = son->FirstAttribute();										//获取子元素的属性
+		if(attr != NULL){
+			do{
+				printf(" %s=%s",attr->Name(),attr->Value());//打印子元素的名字, 值
+			}while(attr->Next());
 		}
-		son = son->NextSiblingElement();							//下一个子元素
+		printf(">");
+
+		son_child = son->FirstChildElement();						//获取子元素的第一个孙元素
+		while(son_child){																//遍历子元素的所有孙元素
+			printf("<%s", son_child->Name());							//打印孙元素的name
+
+			attr = son_child->FirstAttribute();						//获取孙元素的属性
+			if(attr != NULL){
+				do{
+					printf(" %s=%s",attr->Name(),attr->Value());//打印孙元素的名字, 值
+				}while(attr->Next());
+			}
+			printf(">");
+
+			content = son_child->GetText();
+			printf("%s", content);												//打印孙元素的值
+
+			printf("</%s>\n", son_child->Name());
+
+			son_child = son_child->NextSiblingElement();	//下一个孙元素
+		}
+		printf("</%s>\n", son->Name());
+		son = son->NextSiblingElement();								//下一个子元素
 	}
+	printf("</%s>\n", root->Name());
+	// 遍历打印root xml根 end
 
 	return ;
 }
