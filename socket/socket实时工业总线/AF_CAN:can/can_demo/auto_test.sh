06fdb43451a823e5b0d6a4dfe892f85edf682d14
adan-shaw <adan_shaw@qq.com>
2024-06-08 17:51:56 +0800

    update lin

--- socket/socket实时工业总线/AF_CAN:can/can_demo/auto_test.sh
+++ socket/socket实时工业总线/AF_CAN:can/can_demo/auto_test.sh
@@ -8,6 +8,8 @@ gcc -g3 ./can_send.c -o s_can
 
 # 测试环境布置(新增一个ip can虚拟网络空间, 并命名为: vcan0):
 sudo ip link add dev vcan0 type vcan
+# 假设设置为125kbit/s的位速率(这一步可以省略, 如果只是为了仿真模拟CAN 网络的话)
+sudo ip link set vcan0 type can bitrate 125000 
 sudo ip link set up vcan0
 
 
