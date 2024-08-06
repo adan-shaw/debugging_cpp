355cee2565e6e271fb031bab52fa006c7c147c6e
adan-shaw <adan_shaw@qq.com>
2024-08-06 13:52:58 +0800

    update

--- socket/MQ/ZeroMQ/libzmq_demo/client.c
+++ socket/MQ/ZeroMQ/libzmq_demo/client.c
@@ -1,55 +1,55 @@
 #include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
 #include <zmq.h>
 
 int main(void){
+	char szMsg[128] = {0};
+	const char* addr_info = "tcp://127.0.0.1:8888";//使用tcp协议, 目标IP/PORT为127.0.0.1:8888
+	int tmp;
 	void * pCtx = NULL;
 	void * pSock = NULL;
-	//使用tcp协议进行通信, 需要连接的目标机器IP地址为0.0.0.0
-	//通信使用的网络端口 为7766 
-	const char * pAddr = "tcp://0.0.0.0:7766";
 
-
-
-	//创建context 
+	//创建context
 	if((pCtx = zmq_ctx_new()) == NULL){
-		return 0;
+		printf ("zmq_ctx_new(): %s\n", zmq_strerror (errno));
+		return -1;
 	}
 
-	//创建socket 
+	//创建socket(zmq socket目前有6中属性, 这里使用dealer方式)
 	if((pSock = zmq_socket(pCtx, ZMQ_DEALER)) == NULL){
+		printf ("zmq_socket(): %s\n", zmq_strerror (errno));
 		zmq_ctx_destroy(pCtx);
-		return 0;
+		return -1;
 	}
 
-	int iSndTimeout = 5000;// millsecond
-	//设置接收超时 
-	if(zmq_setsockopt(pSock, ZMQ_RCVTIMEO, &iSndTimeout, sizeof(iSndTimeout)) < 0){
+	//设置发送超时(2s)
+	tmp = 2000;// millisecond 毫秒
+	zmq_setsockopt(pSock, ZMQ_SNDTIMEO, &tmp, sizeof(tmp));
+
+	//连接目标IP 127.0.0.1, 端口8888 
+	if(zmq_connect(pSock, addr_info) < 0){
+		printf ("zmq_connect(): %s\n", zmq_strerror (errno));
 		zmq_close(pSock);
 		zmq_ctx_destroy(pCtx);
-		return 0;
+		return -1;
 	}
 
-	//连接目标IP 192.168.1.2, 端口7766 
-	if(zmq_connect(pSock, pAddr) < 0){
-		zmq_close(pSock);
-		zmq_ctx_destroy(pCtx);
-		return 0;
-	}
-
-	//循环发送消息 
+	//循环发送消息(间隔1s) -- server 不在线, 仍然能继续发送, 很神奇(可能跟选择zmq socket 类型有关)
+	tmp = 0;
 	while(1){
-		static int i = 0;
-		char szMsg[1024] = {0};
-		snprintf(szMsg, sizeof(szMsg), "hello world : %3d", i++);
-		printf("Enter to send...\n");
-		if(zmq_send(pSock, szMsg, sizeof(szMsg), 0) < 0)
-		{
-			fprintf(stderr, "send message faild\n");
-			continue;
+		snprintf(szMsg, sizeof(szMsg), "hello world : %3d", tmp++);
+		if(zmq_send(pSock, szMsg, sizeof(szMsg), 0) < 0){
+			printf ("zmq_send(): %s\n", zmq_strerror (errno));
+			break;//zmq_send()超时会报错: Resource temporarily unavailable (zmq 是默认异步的, 不能用死等的老式socket 方式对待)
+			//continue;//但一般情况下, 发送不可能超时
 		}
-		printf("send message : [%s] succeed\n", szMsg);
-		getchar();
+		else
+			printf("client send msg: [%s]\n", szMsg);
+		sleep(1);
 	}
 
+	zmq_close(pSock);
+	zmq_ctx_destroy(pCtx);
 	return 0;
 }
--- socket/MQ/ZeroMQ/libzmq_demo/server.c
+++ socket/MQ/ZeroMQ/libzmq_demo/server.c
@@ -1,53 +1,62 @@
 #include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
 #include <zmq.h>
 
+//只接收10 条消息, 就自动退出, 只做测试
+static unsigned short msg_count = 0;
+
 int main(void){
+	char szMsg[128] = {0};
+	const char* addr_info = "tcp://*:8888";//使用tcp协议, 目标IP/PORT为0.0.0.0:8888
+	int tmp;
 	void * pCtx = NULL;
 	void * pSock = NULL;
-	const char * pAddr = "tcp://*:7766";
 
+	//创建context
+	if((pCtx = zmq_ctx_new()) == NULL){
+		printf ("zmq_ctx_new(): %s\n", zmq_strerror (errno));
+		return -1;
+	}
 
-
-	//创建context, zmq的socket 需要在context上进行创建 
-	if((pCtx = zmq_ctx_new()) == NULL)
-		return 0;
-
-	//创建zmq socket, socket目前有6中属性, 这里使用dealer方式
-	//具体使用方式请参考zmq官方文档(zmq手册) 
+	//创建socket(zmq socket目前有6中属性, 这里使用dealer方式)
 	if((pSock = zmq_socket(pCtx, ZMQ_DEALER)) == NULL){
+		printf ("zmq_socket(): %s\n", zmq_strerror (errno));
 		zmq_ctx_destroy(pCtx);
-		return 0;
+		return -1;
 	}
 
-	int iRcvTimeout = 5000;// millsecond
-	//设置zmq的接收超时时间为5秒 
-	if(zmq_setsockopt(pSock, ZMQ_RCVTIMEO, &iRcvTimeout, sizeof(iRcvTimeout)) < 0){
+	//设置接收超时(2s)
+	tmp = 2000;// millisecond 毫秒
+	zmq_setsockopt(pSock, ZMQ_RCVTIMEO, &tmp, sizeof(tmp));
+
+	//绑定地址 tcp://0.0.0.0:8888
+	if(zmq_bind(pSock, addr_info) < 0){
+		printf ("zmq_bind(): %s\n", zmq_strerror (errno));
 		zmq_close(pSock);
 		zmq_ctx_destroy(pCtx);
-		return 0;
+		return -1;
 	}
+	else
+		printf("bind at: %s\n", addr_info);
 
-	//绑定地址 tcp://*:7766 
-	//也就是使用tcp协议进行通信, 使用网络端口 7766
-	if(zmq_bind(pSock, pAddr) < 0){
-		zmq_close(pSock);
-		zmq_ctx_destroy(pCtx);
-		return 0;
-	}
-	printf("bind at : %s\n", pAddr);
-
+	//循环接收消息(间隔1s)
 	while(1){
-		char szMsg[1024] = {0};
-		printf("waitting...\n");
-		errno = 0;
-		//循环等待接收到来的消息, 当超过5秒没有接到消息时, 
-		//zmq_recv函数返回错误信息, 并使用zmq_strerror函数进行错误定位 
 		if(zmq_recv(pSock, szMsg, sizeof(szMsg), 0) < 0){
-			printf("error = %s\n", zmq_strerror(errno));
-			continue;
+			printf ("zmq_recv(): %s\n", zmq_strerror (errno));
+			//break;//zmq_recv()超时会报错: Resource temporarily unavailable (zmq 是默认异步的, 不能用死等的老式socket 方式对待)
+			continue;//接收可能会超时
 		}
-		printf("received message : %s\n", szMsg);
+		else{
+			printf("server received msg: %s\n", szMsg);
+			msg_count++;
+		}
+		if(msg_count > 10)
+			break;
+		sleep(1);
 	}
 
+	zmq_close(pSock);
+	zmq_ctx_destroy(pCtx);
 	return 0;
 }
