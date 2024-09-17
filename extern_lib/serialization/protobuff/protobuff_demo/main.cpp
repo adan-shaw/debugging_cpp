b3405af06e38ab9eb1c328bad53036160fc285bc
adan-shaw <adan_shaw@qq.com>
2024-09-17 18:19:01 +0800

    update

--- extern_lib/serialization/protobuff/protobuff_demo/main.cpp
+++ extern_lib/serialization/protobuff/protobuff_demo/main.cpp
@@ -3,32 +3,31 @@
 #include <string>
 #include "test.pb.h"
 
-
+#define path_output "./protobuf_file.txt"
 
 int main(void) {
 	// Verify that the version of the library that we linked against is
 	// compatible with the version of the headers we compiled against.
 	GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-	test::pbuff_msg pff_msg;//注意: 此处应该指名namespace = test(在test.proto中指定了)
-	pff_msg.set_m_str("hello,world");
-	pff_msg.set_m_bytes("are you ok?");
+	test::v2msg_demo pvs_msg;//注意: 此处应该指名namespace = test(在test.proto中指定了)
+	pvs_msg.set_m_str("hello,world");
+	pvs_msg.set_m_bytes("are you ok?");
 
-	test::pbuff_msg_em_msg *m_emsg2 = new test::pbuff_msg_em_msg();
+	test::v2msg_demo_emsg *pemsg = new test::v2msg_demo_emsg();
 
-	m_emsg2->set_m_int32(1);
-	m_emsg2->set_m_str("embedded_Info");
-	pff_msg.set_allocated_m_emsg(m_emsg2);
+	pemsg->set_m_int32(1);
+	pemsg->set_m_str("embedded_Info");
+	pvs_msg.set_allocated_m_emsg(pemsg);
 
-	pff_msg.add_repeated_int32(2);
-	pff_msg.add_repeated_int32(3);
-	pff_msg.add_repeated_str("repeated1");
-	pff_msg.add_repeated_str("repeated2");
+	pvs_msg.add_repeated_int32(2);
+	pvs_msg.add_repeated_int32(3);
+	pvs_msg.add_repeated_str("repeated1");
+	pvs_msg.add_repeated_str("repeated2");
 
-	//序列化到file
-	std::string filename = "./protobuf_file.txt";
-	std::fstream output(filename, std::ios::out | std::ios::trunc | std::ios::binary);
-	if (!pff_msg.SerializeToOstream(&output)) {
+	//序列化到file stream
+	std::fstream fs_output(path_output, std::ios::out | std::ios::trunc | std::ios::binary);
+	if (!pvs_msg.SerializeToOstream(&fs_output)) {
 		std::cerr << "Error: " << std::endl;
 		return -1;
 	}
@@ -36,12 +35,15 @@ int main(void) {
 	//序列化到string
 	std::string str_output;
 	try {
-		pff_msg.SerializeToString(&str_output);
+		pvs_msg.SerializeToString(&str_output);
 	}catch (const std::system_error& e) {
 		std::cerr << "Error: " << e.what() << std::endl;
 		return -1;
 	}
+	std::cout << str_output << std::endl;
 
-	delete m_emsg2;
+	pemsg->Clear();
+	pvs_msg.Clear();
+	//delete pemsg;//protobuff 不能直接delete 类实体, protobuff 使用Clear() 清除msg中的数据, 并释放其占用的资源
 	return 0;
 }
